a7cb6302b18a7cd68af3455efe805db86fd227f8
Adrian @ Tentris <adrian@tentris.io>
2026-07-16 15:39:32 +0200

    Include Tentris Org in the Renovation. (#101)

--- scripts/renovate-repos.sh
+++ scripts/renovate-repos.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-env | grep '^DICE_GROUP_REPO_' | cut -d= -f2- | sort | jq -R -s -c 'split("\n") | map(select(length > 0))'
+PREFIX="${1:?PREFIX is not set}"
+
+env | grep "^${PREFIX}_" | cut -d= -f2- | sort | jq -R -s -c 'split("\n") | map(select(length > 0))'
